58dcd8ca442760045f3e2c385b0161a9720ac56a
Joe Vacovsky <vacovsky@gmail.com>
2017-06-17 13:47:11 -0700

    added empty tests

---
+++ detokenizer_test.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mapKeyPairs(t *testing.T) {
+	type args struct {
+		path   string
+		buffer string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string][]byte
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mapKeyPairs(tt.args.path, tt.args.buffer); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mapKeyPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_detokenize(t *testing.T) {
+	type args struct {
+		input    []byte
+		tokenMap map[string][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := detokenize(tt.args.input, tt.args.tokenMap); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("detokenize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_loadFile(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := loadFile(tt.args.path); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_outputToFile(t *testing.T) {
+	type args struct {
+		path string
+		data []byte
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			outputToFile(tt.args.path, tt.args.data)
+		})
+	}
+}
+
+func Test_outputToStdout(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			outputToStdout(tt.args.data)
+		})
+	}
+}
---
+++ tokenExtracter_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_convertToJSON(t *testing.T) {
+	type args struct {
+		data   [][]byte
+		buffer string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertToJSON(tt.args.data, tt.args.buffer); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("convertToJSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_extractTokens(t *testing.T) {
+	type args struct {
+		input  []byte
+		buffer string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]byte
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := extractTokens(tt.args.input, tt.args.buffer); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("extractTokens() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
