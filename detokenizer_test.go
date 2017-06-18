938847e90a141e839889a11bb68fb9cf3de3a68c
Joe Vacovsky <vacovsky@gmail.com>
2017-06-18 10:50:52 -0700

    put a test dataset in

--- detokenizer_test.go
+++ detokenizer_test.go
@@ -93,7 +93,12 @@ func Test_outputToStdout(t *testing.T) {
 		name string
 		args args
 	}{
-	// TODO: Add test cases.
+		{
+			name: "Does it print?",
+			args: args{
+				data: []byte("Does it print?"),
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
