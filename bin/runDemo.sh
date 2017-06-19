e9abfc8bce5cb5811531e359f2606a19bedb3688
Joe Vacovsky <vacovsky@gmail.com>
2017-06-18 22:46:42 -0700

    update demo

--- bin/runDemo.sh
+++ bin/runDemo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-./fasturtle_x64 --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.xml --tokens=tokensDemo.json,tokensDemo2.json
-./fasturtle_x64 --buffer="__" --input=tokenizedDemo.xml --output=myoutput_databag.xml --databag=_default
+go get -u github.com/vacoj/fasturtle
+fasturtle --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.xml --tokens=tokensDemo.json,tokensDemo2.json
+fasturtle --buffer="__" --input=tokenizedDemo.xml --output=myoutput_databag.xml --databag=_default
