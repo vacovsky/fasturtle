f7ef542987c9b9e870500cc89583d485bf97cf00
Joe Vacovsky <vacovsky@gmail.com>
2017-06-19 06:23:50 -0700

    update bin

--- chefKnife.go
+++ chefKnife.go
@@ -5,20 +5,14 @@ import (
 	"log"
 	"os/exec"
 	"strings"
-
-	"fmt"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 // lets you substitute data bags for token files!  So cool!
 // knife data bag show {data_bag} {bag_entry.json} -F json
 
-// TODO: build support for encryptoed data bags
+// TODO: add debugging options so commands are displayed as executed.
 func collectEncrytpedDataBagJSON(bag, blob, secret string) []byte {
-	spew.Dump(bag, blob, secret)
 	cmd := exec.Command("knife", "data", "bag", "show", "--secret-file", secret, bag, blob, "-F", "json")
-	fmt.Println("knife", "data", "bag", "show", "--secret-file", secret, bag, blob, "-F", "json")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
