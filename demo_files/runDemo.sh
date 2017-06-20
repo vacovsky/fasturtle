37c9126bb101d93843d1fde289bde17e6dea8670
Joe Vacovsky <joe.vacovsky@mindbodyonline.com>
2017-06-20 08:51:36 -0700

    merging

--- demo_files/runDemo.sh
+++ demo_files/runDemo.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 go get -u github.com/vacoj/fasturtle
-../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.xml --tokens=tokensDemo.json,tokensDemo2.json
-../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --output=myoutput_databag.xml --databag=_default
-../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --databag=fasturtle --databag-secret=dbsecret --output=results.xml
+
+../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.LR-buffer.xml --output=myoutput_multitokenjson_LR-buffer.xml --tokens=tokensDemo.json,tokensDemo2.json
+../fasturtle --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.xml --tokens=tokensDemo.json,tokensDemo2.json
+# ../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --output=myoutput_databag.xml --databag=_default
+# ../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --databag=fasturtle --databag-secret=dbsecret --output=results.xml
+
+# extract test/demo
+../fasturtle --extract --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.json
+../fasturtle --extract --buffer-left="{{" --buffer-right="}}" --input=tokenizedDemo.LR-buffer.xml --output=myoutput_multitokenjson.LR.json
--- tokenExtracter.go
+++ tokenExtracter.go
@@ -17,7 +17,7 @@ func convertToJSON(data [][]byte, buffer []string) []byte {
 	dmap := map[string]string{}
 	for _, d := range data {
 		cleanD := bytes.Replace(d, []byte(buffer[0]), []byte{}, -1)
-		cleanD = bytes.Replace(d, []byte(buffer[1]), []byte{}, -1)
+		cleanD = bytes.Replace(cleanD, []byte(buffer[1]), []byte{}, -1)
 		dmap[string(cleanD)] = ""
 	}
 	jdata, err := json.Marshal(dmap)
