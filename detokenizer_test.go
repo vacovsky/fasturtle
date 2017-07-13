dbe48f710736f3796fedb6e4b308bf8148ca37e1
Joe Vacovsky <vacovsky@gmail.com>
2017-07-13 07:04:10 -0700

    removed useless tests

--- detokenizer_test.go
+++ detokenizer_test.go
@@ -26,47 +26,6 @@ func Test_mapKeyPairs(t *testing.T) {
 	}
 }
 
-func Test_detokenize(t *testing.T) {
-	type args struct {
-		input    []byte
-		tokenMap []map[string][]byte
-	}
-	tests := []struct {
-		name string
-		args args
-		want []byte
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := detokenize(tt.args.input, tt.args.tokenMap); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("detokenize() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_loadFile(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name string
-		args args
-		want []byte
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := loadFile(tt.args.path); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("loadFile() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func Test_outputToFile(t *testing.T) {
 	type args struct {
 		path string
@@ -81,28 +40,6 @@ func Test_outputToFile(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			outputToFile(tt.args.path, tt.args.data)
-		})
-	}
-}
-
-func Test_outputToStdout(t *testing.T) {
-	type args struct {
-		data []byte
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Does it print?",
-			args: args{
-				data: []byte("Does it print?"),
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			outputToStdout(tt.args.data)
 		})
 	}
 }
