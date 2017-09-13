5ab1b48ad2f21dd02c72668bc3ff15a666dbe57e
Joe Vacovsky <vacovsky@gmail.com>
2017-09-13 10:16:27 -0700

    fixed backslash issue with unquoted configs

--- demo_files/runDemo.sh
+++ demo_files/runDemo.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-go get -u github.com/vacoj/fasturtle
+# go get -u github.com/vacoj/fasturtle
 
-../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.LR-buffer.xml --output=myoutput_multitokenjson_LR-buffer.xml --tokens=tokensDemo.json,tokensDemo2.json
+# ../fasturtle --bufferleft="{{" -buffer-right="}}" --input=tokenizedDemo.LR-buffer.xml --output=myoutput_multitokenjson_LR-buffer.xml --tokens=tokensDemo.json,tokensDemo2.json
 ../fasturtle --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.xml --tokens=tokensDemo.json,tokensDemo2.json
-# ../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --output=myoutput_databag.xml --databag=_default
-# ../fasturtle --buffer-left="{{" -buffer-right="}}" --input=tokenizedDemo.xml --databag=fasturtle --databag-secret=dbsecret --output=results.xml
+# # ../fasturtle --bufferleft="{{" -buffer-right="}}" --input=tokenizedDemo.xml --output=myoutput_databag.xml --databag=_default
+# # ../fasturtle --bufferleft="{{" -buffer-right="}}" --input=tokenizedDemo.xml --databag=fasturtle --databag-secret=dbsecret --output=results.xml
 
-# extract test/demo
-../fasturtle --extract --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.json
-../fasturtle --extract --buffer-left="{{" --buffer-right="}}" --input=tokenizedDemo.LR-buffer.xml --output=myoutput_multitokenjson.LR.json
+# # extract test/demo
+# ../fasturtle --extract --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson.json
+# ../fasturtle --extract --buffer-left="{{" --buffer-right="}}" --input=tokenizedDemo.LR-buffer.xml --output=myoutput_multitokenjson.LR.json
 
 ../fasturtle --buffer="__" --input=tokenizedDemo.xml --output=myoutput_multitokenjson_missing.xml --tokens=tokensDemo_missing.json
 
-../fasturtle --buffer="__" --input=tokenizedDemo.xml --assembly-bindings-source=tokenizedDemoAssemblies.xml --output=myoutput_multitokenjson_assemblies.xml --tokens=tokensDemo.json,tokensDemo2.json
+# ../fasturtle --buffer="__" --input=tokenizedDemo.xml --assembly-bindings-source=tokenizedDemoAssemblies.xml --output=myoutput_multitokenjson_assemblies.xml --tokens=tokensDemo.json,tokensDemo2.json
+# ../fasturtle --buffer="__" --input=tokenizedDemo.xml --assembly-bindings-source=tokenizedDemoAssemblies.xml --output=myoutput_multitokenjson_assemblies.xml --tokens=tokensDemo.json,tokensDemo2.json
+
+../fasturtle --buffer="__"  --input=tokenizedDemoQuoted.xml --output=myoutput_multitokenjson_assemblies_unquoted.xml --tokens=tokensDemo.json,tokensDemo2.json --unquoted
